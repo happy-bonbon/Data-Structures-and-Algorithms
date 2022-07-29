995544951ace8491cb314cc5f308c9ee9699386d
happy-bonbon <happy.yux@gmail.com>
2022-07-29 15:27:58 +1000

    path fix

--- Read File.py
+++ Read File.py
@@ -1,7 +1,8 @@
 # C:\Users\Yuxiao\Desktop\a.txt
 
 # fname = input("Enter file name: ")
-fh = open(r'C:\Users\Yuxiao\Desktop\Code\python\Python-Data-Structures\words.txt')
+# fh = open(r'C:\Users\Yuxiao\Desktop\Code\python\Python-Data-Structures\words.txt')
+fh = open(r'C:\Users\Bonbon\Desktop\Code\Python-Data-Structures\words.txt')
 for line in fh:
     line=line.rstrip().upper()
     # line=line
