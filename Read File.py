5cbf15f82a691ad2f20778e604c7ad9e6d7c3e04
happy-bonbon <happy.yux@gmail.com>
2022-07-29 17:38:55 +1000

    working

--- Read File.py
+++ Read File.py
@@ -2,7 +2,8 @@
 
 # fname = input("Enter file name: ")
 # fh = open(r'C:\Users\Yuxiao\Desktop\Code\python\Python-Data-Structures\words.txt')
-fh = open(r'C:\Users\Bonbon\Desktop\Code\Python-Data-Structures\words.txt')
+# fh = open(r'C:\Users\Bonbon\Desktop\Code\Python-Data-Structures\words.txt')
+fh = open('words.txt')
 for line in fh:
     line=line.rstrip().upper()
     # line=line
