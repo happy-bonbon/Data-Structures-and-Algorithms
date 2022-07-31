11f3bb35677da870b61070dfa27ae435085af292
happy-bonbon <happy.yux@gmail.com>
2022-07-31 22:39:14 +1000

    14 leet code done

---
+++ 14.py
@@ -0,0 +1,42 @@
+'''14. Longest Common Prefix'''
+""" Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings. """
+
+
+strs = ["dog","racecar","car"]
+strs = ["flower", "flow", "flight"]
+strs = ["cir","car"]
+
+
+def longestCommonPrefix(strs):
+    '''assume the shortest word is the prefix, 
+    return its characters one by one until it isn't'''
+    sml = min(strs, key=len)
+    common = ''
+    
+    for i in range(len(sml)):
+        letter = sml[i]
+        for item in strs:
+            if letter != item[i]:
+                letter = ''
+                break
+        else:
+            common += letter
+            continue
+        break
+    return common
+
+
+print(longestCommonPrefix(strs))
