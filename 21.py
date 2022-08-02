f95df779c71dcb7f032bbc51006388261f2d9bf5
happy-bonbon <happy.yux@gmail.com>
2022-08-03 00:37:49 +1000

    21 not correct

--- 21.py
+++ 21.py
@@ -1,14 +1,22 @@
-list1 = [1, 2, 4]
-list2 = [1, 3, 4]
-Output: [1, 1, 2, 3, 4, 4]
+from tkinter import Y
+import numpy as np
+
+# list1 = [1, 2, 4]
+# list2 = [1, 3, 4]
+# Output: [1, 1, 2, 3, 4, 4]
 
 
-def mergeTwoLists(list1, list2):
-    for item in list2:
-        list1.append(item)
-    list1.sort()
+# def mergeTwoLists(list1, list2):
+#     for item in list2:
+#         list1.append(item)
+#     list1.sort()
 
-    return list1
+    # return list1
 
 
-print(mergeTwoLists(list1, list2))
+# print(mergeTwoLists(list1, list2))
+x = np.arange(100)
+y = np.arange(100)
+s = x+y
+print(s)
+print(x)
